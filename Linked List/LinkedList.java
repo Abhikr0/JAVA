aeca59bbe9b65c85d72fb2bdfbf2a13cc6e958b4
Abhikr0 <abhishekkumar.ak2210@gmail.com>
2024-02-23 17:54:12 +0530

    114 lecture

--- Linked List/LinkedList.java
+++ Linked List/LinkedList.java
@@ -1,7 +1,24 @@
 public class LinkedList {
+    public static void displayr(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.data);//to reverse print we have to interchange the line with
+        displayr(head.next);// this one 
+    }
+
+    public static void display(Node head) {
+        // if we use the head as a pointer the change in a 
+        // will make head null
+        while (head!= null) {
+            System.out.println(head.data+"");
+            head = head.next;
+        }
+    }
+    // make temp node to not loose the head value
     public static class Node{
-        int data;// value 
-        Node next;// address of next node
+        int data; // value 
+        Node next; // address of next node
 
         Node(int data){
             this.data = data;// update of the new 
@@ -10,7 +27,7 @@ public class LinkedList {
     public static void main(String[] args) {
         Node a = new Node(5);
         Node b = new Node(3);
-        Node c = new Node(9);
+        Node c = new Node(9);  
         Node d = new Node(8);
         Node e = new Node(10);
 
@@ -19,6 +36,6 @@ public class LinkedList {
         c.next = d;
         d.next = e;
 
-        System.out.println(a.next);
+        displayr(a);
     }
 }
