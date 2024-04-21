74b850c606d768609d09717cfb1291770dfbabca
Abhikr0 <abhishekkumar.ak2210@gmail.com>
2024-04-22 01:33:52 +0530

    up

--- Loops/cont.java
+++ Loops/cont.java
@@ -1,7 +1,7 @@
 
 public class cont {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
+        for (int i=0;i<=5;i++) {
             if(i==3){
                 continue;
             }
